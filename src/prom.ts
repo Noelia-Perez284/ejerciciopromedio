5216e7631a9bdf4527f5c459a34f06ac5597802f
Noelia-Perez284 <102192324+Noelia-Perez284@users.noreply.github.com>
2022-04-04 23:22:04 -0300

    Initial commit

---
+++ src/prom.ts
@@ -0,0 +1,7 @@
+let btnEnviar = document.getElementById("btnEnviar");
+let dato1 = <HTMLInputElement>document.getElementById("dato1");
+let dato2 = <HTMLInputElement>document.getElementById("dato2");
+let dato3 = <HTMLInputElement>document.getElementById("dato3");
+let dato4 = <HTMLInputElement>document.getElementById("dato4");
+
+btnEnviar.addEventListener;
